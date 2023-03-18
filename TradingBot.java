ad7475435147ee7ff32e3fe8eabb21ed3e75c077
BuildTools <unconfigured@null.spigotmc.org>
2023-03-18 16:35:18 -0400

    modified:   TradingBot.java

--- TradingBot.java
+++ TradingBot.java
@@ -1,20 +1,24 @@
 import java.io.*;
 import java.net.*;
+import java.util.Base64;
 
 import org.json.JSONArray;
 
-public class TradingBot { 
+public class TradingBot {
 
     public static final String key = "s9N9ZOUuCoTNJDtlpfvWiHq3_QZhES3S";
 
     public static final String priv = "IGOpMy1NwzKWOjYHgeQQedYspqNb-QHD";
+
     public static void main(String[] args) throws Exception {
-        /*while (true) {
-            System.out.println(getMa());
-            Thread.sleep(50);
-        }*/
+        /*
+         * while (true) {
+         * System.out.println(getMa());
+         * Thread.sleep(50);
+         * }
+         */
         postOrderRequest(true);
-        //TODO figure out when and how much to buy
+        // TODO figure out when and how much to buy
     }
 
     public static void postOrderRequest(boolean buy) throws Exception {
@@ -28,18 +32,27 @@ public class TradingBot {
         String type = "market";
         String timeInForce = "GTC";
 
-
         URL url = new URL("https://api.pro.changelly.com/api/3/margin/order");
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setDoOutput(true);
         con.setRequestMethod("POST");
+        String userpass = key + ":" + priv;
+        String basicAuth = "Basic " + new String(Base64.getEncoder().encode(userpass.getBytes()));
+        con.addRequestProperty("Authorization", basicAuth);
+
         
-        con.setRequestProperty("s9N9ZOUuCoTNJDtlpfvWiHq3_QZhES3S", "IGOpMy1NwzKWOjYHgeQQedYspqNb-QHD");
+        con.addRequestProperty("Content-Type ", "application/json");
 
-//TODO setup the post request to the endpoint.
+        
 
-        //byte[] props = {side.getBytes()}; 
-        //System.out.println(props.toString());
-con.getOutputStream().write(null);
+        String postRequestString = "{\"client_order_id\":\"\", \"symbol\": \"BTCUSDT\", \"side\": \"sell\", \"type\": \"market\", \"time_in_force\": \"GTC\", \"quantity\": \"2\"}";
+
+        String temp = "client_order_id=&time_in_force=GTC&reduce_only=&price=&quantity=2&post_only=&strict_validate=&stop_price=&expire_time=&make_rate=&side=sell&take_rate=&symbol=BTCUSDT&type=market";
+        try (OutputStream os = con.getOutputStream()) {
+            byte[] input = postRequestString.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+        // con.getOutputStream().write(temp.getBytes());
 
         BufferedReader webRespOrder = new BufferedReader(new InputStreamReader(con.getInputStream()));
 
@@ -49,7 +62,9 @@ con.getOutputStream().write(null);
         while ((inputLine = webRespOrder.readLine()) != null) {
             responseTemp.concat(inputLine);
         }
+        System.out.println(responseTemp);
         webRespOrder.close();
+        con.disconnect();
     }
 
     public static double getMa() throws Exception {
@@ -59,7 +74,6 @@ con.getOutputStream().write(null);
 
         BufferedReader webRespMa = new BufferedReader(new InputStreamReader(con.getInputStream()));
 
-        
         String inputLine = null;
         StringBuffer response = new StringBuffer();
         while ((inputLine = webRespMa.readLine()) != null) {
